e31c3fb5ade3b90a111f485368f69a4e353bcae8
Devon Hirth <dhirth@codeforamerica.org>
2025-04-08 17:03:46 -0400

    WIP update config to the latest format. Update readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Figma Application Question Export
 
-Exports component properties, or meta data, from Figma files that use the **Application Question** component to design integrated benefit applications.
+Exports component properties, or meta data, from Figma files that use the **Application Question** component to design integrated benefit applications. This won't work with every Figma form flow project but it can be adapted to extract component data for other projects.
 
 ![Preview](image.png)
 
@@ -10,5 +10,6 @@ Exports component properties, or meta data, from Figma files that use the **Appl
     * Your personal Figma access token.
     * The file key ID to pull from.
     * The node IDs of pages in the file to pull from.
+    * The component to extract from the file.
 
 1. Run the command `node meta.js` to request data and build the export.
--- config.js
+++ config.js
@@ -1,9 +1,23 @@
 module.exports = {
   'TOKEN': '',           // Replace with your personal Figma access token
   'FILE': '',            // Replace with your file key ID
-  'PAGES': [],           // Add the pages in the file you want to extract from
-  'COMPONENTs': [],      // Names of the components you want to pull properties from in each page
+  'PAGES': [             // Add the pages in the file you want to extract from
+    {
+      'ID': ''           // The node ID of the page to retrieve
+    }
+  ],
+  'COMPONENTs': [        // Names of the components you want to pull properties from in each page
+    'Application Question'
+  ],
   'EXPORT': './data',    // File to export data to
-  'HEADINGS': [],        // Column headings for the export
+  'HEADINGS': [          // Column headings for the export
+    'ID',
+    'Required',
+    'Question (New)',
+    'Question (Current)',
+    'Question (Not Changed)',
+    'Flow',
+    'Section'
+  ],
   'DELIMITER': '|'       // Column delimiter
 };
